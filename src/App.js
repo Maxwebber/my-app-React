eb0d01257aaa05e7a3230c502e17fecf85ca3299
igormotta92 <igor.mottta@gmail.com>
2022-05-19 14:31:08 -0300

    Ajustes index.js e componente Blog e Home.jsx

--- src/App.js
+++ src/App.js
@@ -3,61 +3,48 @@ import Home from './components/Home';
 import Sobre from './components/Sobre';
 import Alunos from './components/Alunos';
 import Galeria from './components/Galeria';
+import Blog from './components/Blog';
 import logotrue from "./components/img/logotrue.png";
-import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
-import {Nav, Navbar, Container} from 'react-bootstrap';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { Nav, Navbar, Container } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <h1></h1>
-      
-
+      {/* <h1></h1> */}
       <BrowserRouter>
+        <Navbar bg="light">
+          <Container>
+            <Navbar.Brand href="#home">
+              <img
+                src={logotrue}
+                width="100"
+                height="30"
+                className="d-inline-block align-top"
+                alt="React Bootstrap logo"
+              />
+            </Navbar.Brand>
+          </Container>
 
-      
-      <Navbar bg="light">
-  <Container>
-    <Navbar.Brand href="#home">
-      <img
-        src={logotrue}
-        width="100"
-        height="30"
-        className="d-inline-block align-top"
-        alt="React Bootstrap logo"
-      />
-    </Navbar.Brand>
-  </Container>
-  
-  <Navbar bg="light" variant="light" className="justify-content-end" >
+          <Navbar bg="light" variant="light" className="justify-content-end" >
+            <Nav className="justify-content-end">
+              <Nav.Link as={Link} to="/"  >Home</Nav.Link>
+              <Nav.Link as={Link} to="/Sobre">Sobre</Nav.Link>
+              <Nav.Link as={Link} to="/Alunos">Career</Nav.Link>
+              <Nav.Link as={Link} to="/Galeria">Contact </Nav.Link>
+              <Nav.Link as={Link} to="/blog">Blog </Nav.Link>
+            </Nav>
+          </Navbar>
+        </Navbar>
 
-      <Nav className="justify-content-end">
-
-        <Nav.Link as={Link} to="/"  >Home</Nav.Link>
-        <Nav.Link as={Link} to="/Sobre">Blog</Nav.Link>
-        <Nav.Link as={Link} to="/Alunos">Career</Nav.Link>
-        <Nav.Link as={Link} to="/Galeria">Contact </Nav.Link>
-        <Nav.Link as={Link} to="/blog">blog2 </Nav.Link>
-
-      </Nav>
-
-      </Navbar>
-      </Navbar>
-
-      <Routes>
-        <Route path='/' index element ={<Home/>}></Route>
-        <Route path='/Sobre'  element={<Sobre/>}></Route>
-        <Route path='/Alunos'  element={<Alunos/>}></Route>
-        <Route path='/Galeria'  element={<Galeria/>}></Route>
-        <Route path='/blog'  element={<blog/>}></Route>
-      </Routes>
-
-
+        <Routes>
+          <Route path='/' index element={<Home />}></Route>
+          <Route path='/Sobre' element={<Sobre />}></Route>
+          <Route path='/Alunos' element={<Alunos />}></Route>
+          <Route path='/Galeria' element={<Galeria />}></Route>
+          <Route path='/blog' element={<Blog />}></Route>
+        </Routes>
       </BrowserRouter>
-
-
-
-
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-  
-);
+/**
+ * Corrigindo
+ * Warning: ReactDOM.render is no longer supported in React 18. Use createRoot instead. 
+ * Until you switch to the new API, your app will behave as if it's running React 17. 
+ * Learn more: https://reactjs.org/link/switch-to-createroot
+ */
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
 
