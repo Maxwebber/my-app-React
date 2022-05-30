d16d889df7ec86c44b6d4b1de06dbf4c4a3189d3
Max Paz <maxfranca74@yahoo.com.br>
2022-05-30 15:41:34 -0300

    correção
    retirado o link

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -269,18 +269,22 @@ function Home() {
 
   <CFooter>
   <div className="footer">
-<<<<<<< Updated upstream
-        <img className="logofooter1" src={logotrue} alt="logo" />
+
+        <img 
+        className="logofooter1"
+         src={logotrue} alt="logo"/>
+
         <div className="midias">
-=======
-        <img className="logofooter1" src={logotrue} alt="logo"  />
->>>>>>> Stashed changes
+
+       
+
         <img className="icon5" src={icon5} alt="logo" />
         <img className="icon6"src={icon6} alt="logo" />
         <img className="icon7" src={icon7} alt="logo" />
         </div>
         <img className="iconlogor2" src={logofooter} alt="logo" />
         </div>
+
 </CFooter>
 
   </Container>
